5ba60a3327fc212d747b5f653f6fb58c007d09a0
Cory Torode <cowtorode@gmail.com>
2024-03-21 18:22:17 -0400

    Added 16 ms delay between latches and data queries

--- adapter.cpp
+++ adapter.cpp
@@ -1,33 +1,25 @@
-
-// SNES Controller Read Example for Arduino Nano
-// Reads button states from a SNES controller
-
 #define CLOCK_PIN 2    // Clock pin of the SNES controller connected to Arduino pin 2 (D2)
 #define LATCH_PIN 3    // Latch pin of the SNES controller connected to Arduino pin 3 (D3)
 #define DATA_PIN 4     // Data pin of the SNES controller connected to Arduino pin 4 (D4)
 
 void setup() {
-    Serial.begin(9600);   // Initialize serial communication
+    // Initialize serial communication
+    Serial.begin(9600);
     pinMode(CLOCK_PIN, OUTPUT);  // Set clock pin as output
     pinMode(LATCH_PIN, OUTPUT);  // Set latch pin as output
     pinMode(DATA_PIN, INPUT);    // Set data pin as input
 }
 
 void loop() {
-    // Read button states from SNES controller
     uint16_t input = readSNESController();
+    // wait 16 ms (16.67 ms) between inputs, the SNES was 60 Hz.
+    delay(16);
 
-    // Print button states to Serial Monitor
-    // Serial.print("Button States: ");
-    // Serial.println(input, BIN);  // Print in binary format ( for some reason this prints in 32 bits )
-
-    // send serial data to usb port
-    // send least significant byte first
+    // Send serial data to USB port
+    // Send the least significant byte first, ...
     Serial.write(input);
-    // and then send most significant byte
+    // and then send the most significant byte
     Serial.write(input >> 8);
-
-    // delay(100);  // Delay if to avoid flooding the Serial Monitor
 }
 
 /**
@@ -36,15 +28,18 @@ void loop() {
 uint16_t readSNESController() {
     uint16_t input = 0;
 
+    // Save button inputs in the controller
     digitalWrite(LATCH_PIN, HIGH);
     delayMicroseconds(12);
     digitalWrite(LATCH_PIN, LOW);
     delayMicroseconds(6);
 
+    // Read button inputs
+    // SNES Controller protocol only modifies 12 bits, so we don't need to iterate through the entire 16 bits.
     for (int i = 0; i < 16; i++) {
-        // store each bit ( in reverse order )
-        input |= digitalRead(DATA_PIN) << i;
-        // get next bit
+        // Store each bit (in reverse order)
+        input |= digitalRead(DATA_PIN) << i; // 1111111111111111
+        // Get next bit
         digitalWrite(CLOCK_PIN, HIGH);
         delayMicroseconds(6);
         digitalWrite(CLOCK_PIN, LOW);
